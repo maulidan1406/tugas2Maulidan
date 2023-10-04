2dfc4c51e5910e9825886d0864962ced5c684fe8
maulidan1406 <114299839+maulidan1406@users.noreply.github.com>
2023-10-04 23:00:04 +0700

    Add files via upload

---
+++ maulidan2.js
@@ -0,0 +1,8 @@
+var firstName = "Muhamad"
+var lastName  = "Maulidan"
+var age       = 23;
+var isMarried = false;
+
+console.log("Nama lengkap saya :" + firstName + " " + lastName)
+console.log("Umur saya :" + age)
+console.log("status pernikahan :" + isMarried)
